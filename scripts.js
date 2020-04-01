de561986fd53aa1f4b3202109533f0255b2cfa2a
ClaudioAlbino <claudio.albino.santos@gmail.com>
2020-04-01 07:58:21 -0300

    todas as paginas criadas

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.cardReceita');
 
+
 for (let cardReceita of cards){
     cardReceita.addEventListener("click", function(){
-        /* const videoId = cardReceita.getAttribute("id") */
+               
         modalOverlay.classList.add('active')
-        
+
+        modalOverlay.querySelector("img").src = cardReceita.querySelector("img").src
+        modalOverlay.querySelector("img").alt = cardReceita.querySelector("img").alt
+        modalOverlay.querySelector("h2").innerText = cardReceita.querySelector("h3").innerText
+        modalOverlay.querySelector("p").innerText = cardReceita.querySelector("p").innerText
+
     });
 }
 
